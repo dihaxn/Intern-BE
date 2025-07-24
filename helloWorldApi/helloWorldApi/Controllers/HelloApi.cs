5359b9dce3c7453f89656ef8df06ac27a173642e
dihaxn <ihanlaknuka@gmail.com>
2025-07-24 16:27:07 +0530

    Add 1 task

---
+++ helloWorldApi/helloWorldApi/Controllers/HelloApi.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloWorldApi.Controllers
+{
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloApi: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Sunny");
+    }
+}
